7cd64f8904b7d4f6a13583ed3f703eee387820b7
Oomaryaser <88951305+Oomaryaser@users.noreply.github.com>
2021-11-18 23:38:02 -0800

    Add files via upload

---
+++ SpeakGoodBye.js
@@ -0,0 +1,28 @@
+
+
+(function() {
+
+
+
+
+
+
+    var byeSpeaker = {};
+    
+    var speakWord = "good bay";
+
+
+    byeSpeaker.speak = function (name) {
+      console.log(speakWord + " " + name);
+    }
+
+
+
+
+window.byeSpeaker = byeSpeaker;
+
+})();
+
+
+
+
